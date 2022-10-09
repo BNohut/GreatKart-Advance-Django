8601b4bed4ac4b9b9afbb17b2d37e2092f8df851
Bugra Nohut <bugra.nohut@gmail.com>
2022-10-10 00:04:10 +0300

    allowed host

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ["greatkart-course-env.eba-ncpaa3pq.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["greatkart-course-env.eba-crnaepk3.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
